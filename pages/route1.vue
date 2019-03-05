3b286c3714e8426d9782e3de57a495596b54b161
Colin Baxter <colin.baxter@zaneray.com>
2019-03-05 09:47:01 -0700

    Router testing

---
+++ pages/route1.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>This is route 1</h1>
+</template>
+
+<script>
+  export default {
+    name: "route1"
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ pages/route2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>ROUTE 2 BIATCHES</h1>
+    <nuxt-link to="/">HOME</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "route2"
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
