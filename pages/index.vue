73ba44fd7b3ee20aee624b2bafc7b79f23aa4abb
Colin Baxter <colin.baxter@zaneray.com>
2019-03-05 10:08:11 -0700

    Demonstrating data

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="container">
     <h1>A Clean Nuxt Build</h1>
+    <h2>{{testPurpose}}</h2>
   </section>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      testPurpose: 'Testing devDependencies'
+    }
+  },
   mounted() {
     console.log('!!!mounted on root route')
   }
@@ -18,30 +24,15 @@ export default {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  h1,
+  h2 {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
 </style>
